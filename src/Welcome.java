4bc8b3520da38ef5da5c85001ec17880791aeb8f
hjh2252173 <145317483+hjh2252173@users.noreply.github.com>
2024-03-12 17:15:44 +0900

    2번째 찐찐막 끝!

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class Welcome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("당신의 이름을 입력하세요: ");
+		String name = input.next();
+		
+		System.out.println("연락처를 입력하세요: ");
+		int phone = input.nextInt(); // 숫자만 입력
+			
 		String greeting = "Welcome to Shopping Mall!";
 		String tagline = "Welcome to Book Market!!";
 		
@@ -16,6 +24,10 @@ public class Welcome {
 		System.out.println("3. 장바구니 비우기\t7. 장바구니의 항목 삭제하기");
 		System.out.println("4. 영수증 표시하기\t8. 종료");
 		System.out.println("********************");
+		
+		System.out.println("메뉴 번호를 선택하세요: ");
+		int choice = input.nextInt(); // 숫자만 입력
+		System.out.println(choice + "번호를 선택했습니다: ");
 	}
 
 }
